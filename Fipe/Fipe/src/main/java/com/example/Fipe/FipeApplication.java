656557a7085d20bd2cafe83149450bbf734245c8
Paulo Henrique <101124585+HeyPaulin@users.noreply.github.com>
2024-02-29 21:58:11 -0300

    Add files via upload
    Aula 02.

---
+++ Fipe/Fipe/src/main/java/com/example/Fipe/FipeApplication.java
@@ -0,0 +1,12 @@
+package com.example.Fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example"})
+public class FipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeApplication.class, args);
+	}
+}
